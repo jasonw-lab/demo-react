20d7797d1884f4ebfcf6bd6586ccf6d19a08712a
jason.w <jasonw.lab@gmail.com>
2025-07-02 16:20:40 +0900

    [fix]docker compose file

--- build.sh
+++ build.sh
@@ -9,9 +9,13 @@ export PATH="$VOLTA_HOME/bin:$PATH"
 echo "node version: $(node -v)"
 echo "npm version: $(npm -v)"
 
-
-
-git pull
-#cd  ./backend/docker
-
-docker compose -f docker-compose.yml up -d --build
+# Check if the first argument is "stop"
+if [ "$1" = "stop" ]; then
+  # If the argument is "stop", run the down command
+  docker-compose -f docker-compose.yml down --rmi all
+else
+  # Otherwise, proceed with the normal build process
+  git pull
+  #cd  ./backend/docker
+  docker compose -f docker-compose.yml up -d --build
+fi
